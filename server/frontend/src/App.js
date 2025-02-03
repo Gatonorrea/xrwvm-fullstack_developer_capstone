77a1a401b2193e2b671b1cdb90d32d35f19d97a5
Fabian Santiago Gutierrez <santigutierrez98@gmail.com>
2025-02-03 15:45:05 -0500

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,19 @@
-import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoginPanel from './components/Login/Login'; // Import the LoginPanel component
 
 function App() {
   return (
     <Routes>
+      {/* Route for the login page */}
       <Route path="/login" element={<LoginPanel />} />
+
+      {/* Add a default route for the root URL */}
+      <Route path="/" element={<LoginPanel />} />
+
+      {/* Add more routes here as needed */}
     </Routes>
   );
 }
+
 export default App;
